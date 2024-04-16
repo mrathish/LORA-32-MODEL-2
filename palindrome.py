1e015c0d40239e8a39c415dc509eca4a9cc0a65f
mrathish <139150287+mrathish@users.noreply.github.com>
2024-04-16 12:58:42 +0530

    Add files via upload

---
+++ palindrome.py
@@ -0,0 +1,24 @@
+def factorial(n):
+   if n=0
+      return 1
+   else:
+     return n*factorial(n-1)
+
+
+def is an palindrome(n)
+ return str(n)=str(n)[::-1]
+
+num=int(input("enter the integer:")
+    if num%2!=0
+        print("its odd number")
+        fact=factorial(num)
+        num_digit=len(str(fact))
+        print(the factorial is {num} and {fact} and it has {num_digit}digital
+              
+              
+    else:
+        print("its even number")
+        if is palindrome(num):
+            print("{num} is a palindrome num")
+        else:
+            print("{num} is a not palindrome num")
